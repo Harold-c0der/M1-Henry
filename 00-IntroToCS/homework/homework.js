e7343976231d150ea7675b3ec226a8192f33e5f3
Harold <harold.tenorio@epn.edu.ec>
2022-04-25 14:54:24 -0500

    commit

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,13 +1,39 @@
 "use strict";
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-  // algo
+
+  let fun = numb => Number(numb);
+  let arr = Array.from(String(num), fun);
+  let arr1 = arr.reverse();
+  let arr2 = []
+  for (let i = 0; i < arr1.length; i++) {
+    arr2.push(Math.pow(2, i) * arr1[i]) 
+  }
+
+  //sumando los valores del array
+  let result1 = arr2.reduce((acumulador,numero) =>{
+        return acumulador + numero
+  },0);
+
+
+  return result1
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let result = '';
+  while (num != 0 ) {
+      result = (num%2) + result
+      num = Math.trunc(num/2)
+  }
+  return result
 }
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,

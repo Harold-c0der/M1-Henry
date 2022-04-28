6d6534eaf76b64c50035a88b28e506fb49b56ebf
Harold <harold.tenorio@epn.edu.ec>
2022-04-28 14:04:45 -0500

    mejora de fibonacci

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -16,19 +16,7 @@ function nFactorial(n) {
 
 
 function nFibonacci(n) {
-  // Secuencia de Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,…
-  // Retorna el enésimo numero de la serie
-  // nFibonacci(0) // 0  // el elemento 0 es cero
-  // nFibonacci(1) // 1 // el elemento 1 es 1
-  // nFibonacci(6) // 1 // el elemento 6 es 8
-  // let fibo  = [0,1]
-  // for (let i = 2; i <= n ; i++) {
-  //   fibo[i] = fibo[i - 1] + fibo[i - 2] 
-  // }
-  // return fibo[n]
-  if(n === 0 ) return 0
-  else if ( n === 1) return 1
-  return nFibonacci(n - 2) + nFibonacci(n - 1)
+  return (n < 2 && n >= 0) ? n : nFibonacci(n -2) + nFibonacci (n-1)
 }
 
 

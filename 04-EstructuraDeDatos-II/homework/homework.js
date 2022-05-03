8f3c975e38a5a688371abc0bb41d0504fd2860ea
Harold <harold.tenorio@epn.edu.ec>
2022-05-03 09:25:44 -0500

    terminamos homework ed 05

--- 04-EstructuraDeDatos-II/homework/homework.js
+++ 04-EstructuraDeDatos-II/homework/homework.js
@@ -10,24 +10,104 @@
 // remove():   Head --> null y devuelve 1
 // search: Busca un valor dentro de la lista. Puede recibir un valor o una función. Si no hubiera resultados, devuelve null.
 
+class Node{
+  constructor(value,next){
+    this.value = value;
+    this.next = null
+  }
+}
 class LinkedList {
     constructor(){
-      this.xlist = {};
+      this.head = null;
+      this._length = 0;
     }
-    add(){
-
+    add(x){
+      let newNodo = new Node(x,null);
+      let current = this.head
+      if (!current) {
+        this.head = newNodo
+        this._length ++
+        return newNodo
+      }
+      while (current.next) {
+        //distinto de null
+        current = current.next
+      }
+      ////////////////////////
+      current.next = newNodo
+      this._length++;
+      return newNodo
     };
+
     remove(){
-
+      let current = this.head
+      let del;
+      this._length --
+      if (!current) {
+        return null
+      }
+      if(current.next === null) {
+        del = this.head.value
+        delete this.head
+        return del
+      }
+      while (current.next.next) { 
+        //distinto de null
+        current = current.next
+      }
+      ////////////////////
+      del = current.next.value
+      console.log(del);
+      current.next = null
+      return del
     };
-    search(){
 
+    search(x){
+      if(this.head === null ) return null;
+      let current = this.head;
+      while(current){
+      if(current.value === x) return current.value
+      else if(typeof x == 'function'){
+        if(x(current.value)){
+          return current.value;
+        }
+      }
+        current = current.next
+    }
+      return null;
     };
+
+    arr(){
+      let arry = []
+      let current = this.head
+      while (current != null) {
+        arry.push(current.value)
+        current = current.next
+      } 
+      return arry
+    }
+
+    size(){
+      return this._length
+    }
 }
 
-function Node(value){
 
-}
+let lista = new LinkedList()
+lista.add(2)
+lista.add('two')
+lista.add(2)
+console.log(lista.size())
+// let persona = new UserNode('Harold', 'zzz','madrid')
+// console.log(persona.name);
+// lista.add(new UserNode('Harold', 'zzz','madrid'))
+lista
+console.log(lista.search(2))
+console.log(lista.search())
+console.log(lista.search(function(nodeValue) {
+  return nodeValue === 'two';
+}))
+
 
 // Hash Table( ver información en: https://es.wikipedia.org/wiki/Tabla_hash)
 // Una Hash table contiene un arreglo de "contenedores" o buckets donde puede guardar información.
@@ -41,9 +121,36 @@ function Node(value){
 //    - Usar el número obtenido, para buscar(llamando al método get) el contenedor o bucket donde está el valor.
 //    - Retornar dicho valor.
 
-function HashTable() {
-
-}
+class HashTable {
+    constructor(){
+      this.numBuckets = 35;
+      this.contenedores = [];
+    }
+    
+    set(key,value){
+      if (typeof key != "string") {
+      throw new TypeError("La key debe ser un string");
+      
+      };
+      var posicion = this.hash(key);
+      this.contenedores [posicion] = this.contenedores[posicion] || [];
+      this.contenedores [posicion].unshift({ key: key, value: value });
+    }
+    get(value){
+      let numeroDelContenedor = this.hash(value);
+      for (let i = 0; i < this.contenedores[numeroDelContenedor].length; i++) {
+        if (this.contenedores[numeroDelContenedor][i].key === value) {
+          return this.contenedores[numeroDelContenedor][i].key;
+        }
+        
+      }
+      return false
+    }
+    hasKey(key){
+      let aux = this.get(key)
+      return aux ?  true : false 
+    }
+};
 
 
 // No modifiquen nada debajo de esta linea
--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -8,10 +8,121 @@
 // corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"
 // corre breadth-first cuando breadthFirstForEach() es ejecutado
 // Observar imagen de la carpeta "homework" llamada "bst.png". Allí encontraran dibujado el arbol utilizado para los tests
-function BinarySearchTree() {
+class BinarySearchTree {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null
+  };
+  insert(x, y = new BinarySearchTree(x)){
+    // if(!y) let y = this;
+    if (x < this.value) {
+      console.log(this.value)
+      console.log(x)
+      if(this.left === null){ // si left no tiene valor
+        this.left = new BinarySearchTree(x)
+      } else {  // si left ya tiene un valor
+        console.log(this.left);
+        this.left.insert(x)
+      }
+    }
+    if(x >= this.value){
+      if(this.right === null){
+        this.right = new BinarySearchTree(x)
+      }else { 
+        this.right.insert(x)
+      }
+    }
+  };
 
+
+  contains(valor){
+    if(valor === this.value ) return true //encontre el valor ?
+    if (valor > this.value) { 
+      if(!this.right){
+        return false;
+      }
+      else{
+        return this.right.contains(valor)
+      }
+
+    }else{
+      if(!this.left){
+        return false;
+      }
+      else {
+        return this.left.contains(valor)
+      }
+    };
+  //   if (this.value === valor) return true;
+  //   if (valor > this.value) {
+  //   if (!this.right) {
+  //     return false;
+  //   } else {
+  //     return this.right.contains(valor);
+  //   }
+  // } else {
+  //   if (!this.left) {
+  //     return false;
+  //   } else {
+  //     return this.left.contains(valor);
+  //   }
+  // }
 }
-
+      
+  
+  
+  depthFirstForEach(cb , recorrido){
+    if(recorrido === 'pre-order'){
+      cb(this.value)
+      if(this.left){
+        this.left.depthFirstForEach(cb,recorrido)
+      }
+      if(this.right){
+        this.right.depthFirstForEach(cb,recorrido)
+      }
+    }else if(recorrido === 'post-order'){
+      
+        if(this.left){
+          this.left.depthFirstForEach(cb,recorrido)
+        }
+        if(this.right){
+          this.right.depthFirstForEach(cb,recorrido)
+        }
+        cb(this.value)
+    } else{
+        if(this.left){
+        this.left.depthFirstForEach(cb,recorrido)
+        }
+        cb(this.value)
+      
+        if(this.right){
+        this.right.depthFirstForEach(cb,recorrido)
+        }//recorrido order 
+      }
+  };
+  breadthFirstForEach(cb, array ){
+    if(!array){
+      var array = [];
+    }
+    if(this.left){
+      array.push(this.left)
+    }
+    if(this.right){
+      array.push(this.right)
+    }
+    cb(this.value)
+    if(array.length > 0){
+      array.shift().breadthFirstForEach(cb, array)
+    }
+  };
+  size(){
+    if(!this.left && !this.right)return 1
+    else if (this.left && this.right) return 1 + this.left.size() + this.right.size()
+    else if(this.left) return 1 + this.left.size()
+    else if (this.right) return 1 + this.right.size()
+  };
+}
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 

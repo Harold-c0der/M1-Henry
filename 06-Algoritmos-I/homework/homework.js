fba6ab7b1cfb669135d0288dd962860dc33aad73
Harold <harold.tenorio@epn.edu.ec>
2022-05-04 08:57:49 -0500

    Terminado hk algoritmos 1

--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let arr = [1]
+  let i = 2
+  while(num != 1){ // no funcionaba pok cada vez que llamaba while se redeclraba let i
+    if(num % i === 0 ){
+      arr.push(i)
+      num = num / i 
+    } else{
+      i++
+    }
+  } 
+  return arr
 }
 
 function bubbleSort(array) {
@@ -14,16 +24,40 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let cambio = true 
+  while (cambio) {
+    cambio = false
+    for(let i = 0; i < array.length ; i++){
+      if(array[i] > array[i + 1]){
+        let mas = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = mas
+        cambio = true
+      } 
+    }
+    
+  }
+  
+  return array
 }
-
-
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  // [5, 1, 4, 2, 8]
+   // x  i
+  //     aux
 
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i]
+    let j;// elemento a insertar
+    for (j = i - 1; j >= 0 && array[j] > current ; j--) {
+      array[j + 1] = array[j]
+    }
+    array[j + 1] = current;
+  }
+  return array
 }
 
 
@@ -32,7 +66,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let minimo = i
+      for (let x = i + 1; x < array.length; x++) {
+        if (array[x] < array[minimo]) {
+          minimo = x
+      }
+    }
+    if(minimo !== i ){
+      let aux = array[i]
+      array[i] = array[minimo]
+      array[minimo] = aux
+    }
+  }
+  return array  
 }
 
 
